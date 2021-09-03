eadb288946b8958b242e8ebc05aa0d899b36c6e0
Jaime Haddad <jhad7010@gmail.com>
2021-09-03 17:20:40 -0600

    First chance / commit from Visual Studio

--- progitforprogrammers/ProGitForProgrammers/Program.cs
+++ progitforprogrammers/ProGitForProgrammers/Program.cs
@@ -11,6 +11,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
             Console.ReadLine();
         }
     }
